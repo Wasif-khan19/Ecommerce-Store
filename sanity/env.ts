5ecd15dbc5a330dcdf84ae80c928474ead70d8b4
Wasif <wk5599426@gmail.com>
2023-09-08 20:59:25 +0500

    modified

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-01'
 
-  
+
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
@@ -20,4 +20,5 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
   }
 
   return v
+  
 }
